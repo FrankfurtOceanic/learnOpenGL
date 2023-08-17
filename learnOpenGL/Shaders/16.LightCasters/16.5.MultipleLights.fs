caf877d74991131c8e5098cd9af1dcb14cb163e5
Joey Marten <joey.marten@mail.mcgill.ca>
2023-08-17 13:37:37 -0400

    Added full support for multiple lights
    Current scene has 4 point lights, a directional light, and a spotlight

--- learnOpenGL/Shaders/16.LightCasters/16.5.MultipleLights.fs
+++ learnOpenGL/Shaders/16.LightCasters/16.5.MultipleLights.fs
@@ -10,33 +10,133 @@ struct Material {
 
     float shininess;
 };
+uniform Material material;
 
-struct Light {
-vec3 position;
-vec3 direction;
-
-float cutoff;
-float outerCutoff;
-
-float constant;
-float linear;
-float quadratic;
-
-vec3 ambientStrength;
-vec3 diffuseStrength;
-vec3 specularStrength;
+//Directional light
+struct DirLight
+{
+    vec3 direction;
+    vec3 ambientStrength;
+    vec3 diffuseStrength;
+    vec3 specularStrength;
 };
+uniform DirLight dirLight;
+
+//PointLight
+struct PointLight
+{
+    vec3 position;
+
+    float constant;
+    float linear;
+    float quadratic;
+
+    vec3 ambientStrength;
+    vec3 diffuseStrength;
+    vec3 specularStrength;
+};
+#define NR_POINT_LIGHTS 4
+uniform PointLight pointLights[NR_POINT_LIGHTS];
+
+//SpotLight
+struct SpotLight {
+    vec3 position;
+    vec3 direction;
+
+    float cutoff;
+    float outerCutoff;
+
+    float constant;
+    float linear;
+    float quadratic;
+
+    vec3 ambientStrength;
+    vec3 diffuseStrength;
+    vec3 specularStrength;
+};
+uniform SpotLight spot;
+
 
 in vec3 Norm;
 in vec3 FragPos;
 
-uniform Material material;
-uniform Light light;
 
 uniform vec3 viewPos;
 
+//functions
+vec3 CalculateDirectionalLight(DirLight dLight, vec3 normal, vec3 viewDir);
+vec3 CalculatePointLight(PointLight light, vec3 normal, vec3 viewDir, vec3 fragPos);
+vec3 CalculateSpotLight(SpotLight light, vec3 normal, vec3 viewDir, vec3 fragPos);
 
 void main()
+{
+    //calculate view direction 
+    vec3 viewDirection = normalize(viewPos - FragPos);
+
+    //unit length normal vector
+    vec3 norm = normalize(Norm);
+    
+    // define an output color value
+    vec3 output = vec3(0.0);
+
+    //add directional Light
+    output += CalculateDirectionalLight(dirLight, norm, viewDirection);
+
+    //add point lights
+    for(int i = 0; i < NR_POINT_LIGHTS; i++)
+    {
+        output += CalculatePointLight(pointLights[i], norm, viewDirection, FragPos);
+    }
+
+    // and add others lights as well (like spotlights)
+    output += CalculateSpotLight(spot, norm, viewDirection, FragPos);
+
+    FragColor = vec4(output, 1.0);
+}
+
+vec3 CalculateDirectionalLight(DirLight dLight, vec3 normal, vec3 viewDir)
+{
+    vec3 ambient = dLight.ambientStrength * vec3(texture(material.diffuseMap, TexCoord));
+
+    //diffuse
+    vec3 lightDirection = normalize(-dLight.direction);
+    float diffStrength = max(dot(lightDirection, normal), 0.0);
+    vec3 diffuse = (diffStrength * vec3(texture(material.diffuseMap, TexCoord)) ) * dLight.diffuseStrength;
+
+    //specular
+    vec3 reflectDir = reflect(-lightDirection, normal);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    vec3 specular = (spec *vec3(texture(material.specularMap, TexCoord))) * dLight.specularStrength;
+
+    return ambient + diffuse + specular;
+}
+
+vec3 CalculatePointLight(PointLight light, vec3 normal, vec3 viewDir, vec3 fragPos)
+{
+    //attenuation calculation
+
+    float distance = length(fragPos - light.position);
+    float attenuation = 1 / (light.constant + light.linear * distance + light.quadratic * distance * distance);
+    //ambient
+    vec3 ambient = light.ambientStrength * vec3(texture(material.diffuseMap, TexCoord));
+
+    //diffuse
+    vec3 lightDirection = light.position - fragPos;
+    lightDirection = normalize(lightDirection);
+    float diffStrength = max(dot(lightDirection, normal), 0.0);
+    vec3 diffuse = (diffStrength * vec3(texture(material.diffuseMap, TexCoord)) ) * light.diffuseStrength;
+
+    //specular
+
+    vec3 reflectDir = reflect(-lightDirection, normal);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    vec3 specular = (spec *vec3(texture(material.specularMap, TexCoord))) * light.specularStrength;
+
+    //output
+    return (ambient + diffuse + specular) * attenuation;
+}
+
+vec3 CalculateSpotLight(SpotLight light, vec3 normal, vec3 viewDir, vec3 fragPos)
 {
     vec3 lightDirection =  normalize(light.position - FragPos);
 
@@ -68,7 +168,5 @@ void main()
     specular *= intensity;
 
     //output
-    FragColor = vec4((ambient + diffuse + specular)* attenuation, 1.0);
-    //FragColor = vec4(1.0);
-   
+    return (ambient + diffuse + specular)* attenuation;
 }
